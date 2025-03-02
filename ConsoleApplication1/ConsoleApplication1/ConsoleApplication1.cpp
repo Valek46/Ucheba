d131fe97942133857161c8ca2f9b8b41a174e282
Valek46 <132743721+Valek46@users.noreply.github.com>
2025-03-02 17:40:09 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <sstream> 
+using namespace std;
+
+int main() {
+    string input; 
+    double num1, num2; 
+    char operation; 
+
+  
+    cout << "Введите выражение (например, 5 + 3): ";
+    getline(cin, input); 
+
+   
+    stringstream ss(input);
+    ss >> num1 >> operation >> num2;
+
+    
+    if (ss.fail()) {
+        cout << "Ошибка: неверный формат ввода." << endl;
+        return 1; 
+    }
+
+   
+    switch (operation) {
+    case '+':
+        cout << "Результат: " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << "Результат: " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << "Результат: " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 != 0) {
+            cout << "Результат: " << num1 / num2 << endl;
+        }
+        else {
+            cout << "Ошибка: деление на ноль невозможно." << endl;
+        }
+        break;
+    default:
+        cout << "Ошибка: неверная операция." << endl;
+        break;
+    }
+
+    return 0;
+}
